b037a306456c2917bcd80b9e1d630e4df6244b53
Jesica <jessikareix20@gmail.com>
2023-01-10 21:36:33 +0100

    workflow-Jessica-Reche-Server

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-//express
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World! ' + new Date());
     }
 );
 
-//escucha en el puerto 3000
 app.listen(3000,  () => {
     console.log('Example app listening on port 3000!');
     }
