29be1bcd11443bd8c8e38dc5bac60cf1e1fe83b2
Jessica <jessikareix20@gmail.com>
2023-01-11 16:16:16 +0100

    workfloe-test-jest

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const sum = require("./sum");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+let test = test("adds 1 + 2 to equal 3", () => {
+ let expect= expect(sum(1, 2)).toBe(3);
 });
