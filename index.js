6c9df87bb943ac69c98161bdd8276a921ebdfe7e
Jessica-Reche <you@example.com>
2023-01-14 23:24:00 +0100

    workflow-telegram-bot

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const Telegram = require('node-telegram-bot-api');
-const core = require('@actions/core');
+const { core, setFailed } = require('@actions/core');
 const github = require('@actions/github');
 
-async function run() {
+//get the chat id from the bot
+
+
+
+
+async function sendTelegramMessage(telegramToken, chatId, message) {
     try {
-        // Get the Telegram token from the input
-        const telegramToken = core.getInput('TELEGRAM_TOKEN');
-        // Create a new Telegram bot
-        const bot = new Telegram(telegramToken, { polling: true });
-        bot.getUpdates().then(updates => {
-            const chatId = updates[0].message.chat.id;
-            console.log(chatId);
-            const nombre = updates[0].message.chat.first_name;
-
-            const message = `Workflow ejecutado correctamente tras el último commit. Saludos ${nombre}`;
-            bot.sendMessage(chatId, message);
-            // Set the output result variable
-            core.setOutput("RESULT", "Mensaje enviado")
-        });
-        //nombre
-
-
-
-
-
         // Get the commit SHA
         const context = github.context;
         const sha = context.sha;
@@ -31,9 +16,25 @@ async function run() {
         const owner = context.repo.owner;
         console.log(`El último commit en el repositorio ${repo} de ${owner} tiene el sha: ${sha} y el id del chat es: ${chatId}`);
 
+        const bot = new TelegramBot(telegramToken, { polling: true });
+        await bot.sendMessage(chatId, message);
+        core.setOutput("RESULT", "Mensaje enviado")
+
+
     } catch (error) {
-        core.setFailed(error.message);
+        setFailed(error.message);
     }
 }
 
-run();
+
+bot.getChat('JessBot').then(chat => {
+    const chatId = chat.id;
+    // Enviar el ID del chat en el chat
+    bot.sendMessage(chatId, `El ID del chat es: ${chatId}`);
+    const telegramToken = core.getInput('TELEGRAM_TOKEN');
+    const message = `Workflow ejecutado correctamente tras el último commit. Saludos ${nombre}`;
+
+    sendTelegramMessage(telegramToken, telegramChatId, message);
+});
+
+
