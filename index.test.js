014b66ebf001626d540fe3d5ab0502c780af5ed1
Jessica <jessikareix20@gmail.com>
2023-01-24 17:53:45 +0100

    workflow-meme-readme

--- index.test.js
+++ index.test.js
@@ -1,9 +1,6 @@
 //test que comprueba que la estructura del archivo esta bien
 test("Comprueba que el archivo index.js tiene la estructura correcta", () => {
-  const index = fs.readFileSync("index.js", "utf-8");
-  expect(index).toMatch(/const fs = require\("fs"\);/);
-  expect(index).toMatch(/const meme = require\("nodejs-meme-generator"\);/);
-  expect(index).toMatch(/const core = require\("@actions\/core"\);/);
+
   expect(index).toMatch(/async function run\(\) {/);
   expect(index).toMatch(/const frase_positiva = core.getInput\("frase_positiva"\);/);
   expect(index).toMatch(/const frase_negativa = core.getInput\("frase_negativa"\);/);
