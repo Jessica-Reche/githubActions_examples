30fbaaaaecf4345726cf78b9b682fed4e3c922d1
Jessica <jessikareix20@gmail.com>
2023-01-24 17:52:30 +0100

    workflow-meme-readme

--- index.test.js
+++ index.test.js
@@ -8,15 +8,4 @@ test("Comprueba que el archivo index.js tiene la estructura correcta", () => {
   expect(index).toMatch(/const frase_positiva = core.getInput\("frase_positiva"\);/);
   expect(index).toMatch(/const frase_negativa = core.getInput\("frase_negativa"\);/);
   expect(index).toMatch(/const resultado_tests = core.getInput\("resultado_tests"\);/);
-  expect(index).toMatch(/let texto;/);
-  expect(index).toMatch(/if \(resultado_tests === 'success'\) {/);
-  expect(index).toMatch(/texto = frase_positiva;/);
-  expect(index).toMatch(/} else {/);
-  expect(index).toMatch(/texto = frase_negativa;/);
-  expect(index).toMatch(/meme.generate\("your_text", texto\).then\(url => {/);
-  expect(index).toMatch(/let readme = fs.readFileSync\("readme.md", "utf-8"\);/);
-  expect(index).toMatch(/readme += `\n![meme]\(\${url}\)`;/);
-  expect(index).toMatch(/fs.writeFileSync\("readme.md", readme\);/);
-  expect(index).toMatch(/console.log\("Meme añadido al readme"\);/);
-  expect(index).toMatch(/run\(\);/);
 });
