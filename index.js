e4566f43b8cc68f20abecf712beb975d5db03517
Jessica <jessikareix20@gmail.com>
2023-01-24 17:29:24 +0100

    workflow-meme-readme

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const fs = require('fs');
-const nodeMemes = require('node-memes');
+const fs = require("fs");
+const meme = require("nodejs-meme-generator");
+const core = require("@actions/core");
 
-async function run(params) {
-  let { frase_positiva, frase_negativa, resultado_tests } = params;
-  let text;
-  if(resultado_tests === 'success'){
-    text = frase_positiva;
-  }else{
-    text = frase_negativa;
-  }
-  if (!fs.existsSync('readme.md')) {
-    fs.writeFileSync('readme.md', '# README\n');
-  }
-  let meme = await nodeMemes.meme('doge',text,text);
-  fs.appendFileSync('readme.md', '\n' + meme);
-  return 'Meme añadido al readme';
+
+
+if (!fs.existsSync("readme.md")) {
+  fs.writeFileSync("readme.md", "");
 }
 
-module.exports = {
-  run: run
+async function run() {
+  const frase_positiva = core.getInput("frase_positiva");
+  const frase_negativa = core.getInput("frase_negativa");
+  const resultado_tests = core.getInput("resultado_tests");
+  let texto;
+  if (resultado_tests === 'success') {
+    texto = frase_positiva;
+  } else {
+    texto = frase_negativa;
+  }
+
+  meme.generate("your_text", texto).then(url => {
+    let readme = fs.readFileSync("readme.md", "utf-8");
+    readme += `\n![meme](${url})`;
+    fs.writeFileSync("readme.md", readme);
+    console.log("Meme añadido al readme");
+    //Depende del resultado se ejecuta una cosa u otra
+  });
 }
+
+run();
--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { run } = require('./index');
 
+
 describe('Custom action meme', () => {
   beforeEach(() => {
     if (fs.existsSync('readme.md')) {
