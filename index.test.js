4076dccc2d78c9f2e5739f8006b4a94ea8c92517
Jessica <jessikareix20@gmail.com>
2023-01-25 15:37:18 +0100

    workflow-action-readme

--- index.test.js
+++ index.test.js
@@ -6,4 +6,6 @@ test("run", () => {
   run();
   //comprobamos que se ha ejecutado
   expect(run).toHaveBeenCalled();
+  //Comprobamos que la variable resultado_tests es igual a success y si no es igual vemos el error
+  expect(resultado_tests).toEqual("success").catch(e => console.log(e));
 });
