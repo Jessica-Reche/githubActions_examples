981b006a7f3ddb1e191c493a5a1418bcd08cc5e6
Jessica <jessikareix20@gmail.com>
2023-01-26 23:45:18 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -44607,9 +44607,9 @@ async function run(frase_positiva, frase_negativa) {
     if (Number(core.getInput("resultado_tests")) === 0) {
       texto_superior = frase_positiva.split("\n")[0];
       texto = 'Los tests han funcionado y lo sabes';
-
-    } else if(resultado_tests === undefined || resultado_tests === "" || resultado_tests === null || !resultado_tests )
-     {
+      texto_inferior = frase_positiva.split("\n")[1];
+      
+    } else {
       texto = 'Los tests han fallado y lo sabes';
       texto_superior = frase_negativa.split("\n")[0];
       texto_inferior = frase_negativa.split("\n")[1];
