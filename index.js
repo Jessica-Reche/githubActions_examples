dc91506f468cf1c3484834550267e1bdf9faa0f8
Jessica <jessikareix20@gmail.com>
2023-01-26 17:53:18 +0100

    workflow-action-readme

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function run(frase_positiva, frase_negativa, resultado_tests) {
   let texto_inferior;
   let texto;
   try {
-    if (resultado_tests === 0 || resultado_tests === '0') {
+    if (resultado_tests === 0 || resultado_tests === '0' || resultado_tests === 'false' || resultado_tests === false || resultado_tests === 'no' || resultado_tests === 'NO' || resultado_tests === 'No' || resultado_tests === 'nO') {
       texto_superior = frase_positiva.split("\n")[0];
       texto = 'frase_positiva';
       texto_inferior = frase_positiva.split("\n")[1];
