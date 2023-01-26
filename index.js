fccae012d9f82f8841706654aee40fa576b3751b
Jessica <jessikareix20@gmail.com>
2023-01-26 13:40:08 +0100

    workflow-action-readme

--- index.js
+++ index.js
@@ -3,26 +3,13 @@ const {memeAsync} = require("memejs");
 const core = require("@actions/core");
 
 async function run() {
-  try {
-//acceder desde las variables de entorno
-    const frase_positiva = core.getInput("frase_positiva");
-    const frase_negativa = core.getInput("frase_negativa");
-    const resultado_tests = core.getInput("resultado_tests");
-
-    console.log("frase_positiva: ", frase_positiva);
-    console.log("frase_negativa: ", frase_negativa);
-    console.log("resultado_tests: ", resultado_tests);
-    generar_meme(frase_positiva, frase_negativa, resultado_tests);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-function generar_meme(frase_positiva, frase_negativa, resultado_tests) {
+  const frase_positiva = core.getEnv("frase_positiva");
+  const frase_negativa = core.getEnv("frase_negativa");
+  const resultado_tests = core.getInput("resultado_tests");
   let texto_superior;
   let texto_inferior;
   let texto;
-  if (resultado_tests == 0) {
+  if (resultado_tests === 'success') {
     texto_superior = frase_positiva.split("\n")[0];
     texto=frase_positiva;
     texto_inferior = frase_positiva.split("\n")[1];
