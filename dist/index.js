8511604d0956153240069564a32168e6be8e201d
Jessica <jessikareix20@gmail.com>
2023-01-26 21:26:45 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -44591,44 +44591,31 @@ const { memeAsync } = __nccwpck_require__(3062);
 const core = __nccwpck_require__(6643);
 const frase_positiva = core.getInput("frase_positiva");
 const frase_negativa = core.getInput("frase_negativa");
-const resultado_tests = core.getInput("resultado_tests");
+
 
 
 run(frase_positiva, frase_negativa, resultado_tests);
 
-async function run(frase_positiva, frase_negativa, resultado_tests) {
+async function run(frase_positiva, frase_negativa) {
 
   let texto_superior;
   let texto_inferior;
- 
+
   try {
 
     let texto = false;
     if (Number(core.getInput("resultado_tests")) === 0) {
-     
       texto_superior = frase_positiva.split("\n")[0];
-      texto = true;
+      texto = 'Los tests han pasado y lo sabes';
       texto_inferior = frase_positiva.split("\n")[1];
-    } else if (Number(core.getInput("resultado_tests")) === 1) {
-      texto = false;
+    } else {
+      texto = 'Los tests han fallado y lo sabes';
       texto_superior = frase_negativa.split("\n")[0];
       texto_inferior = frase_negativa.split("\n")[1];
-    } else {
-      //se guarda en la variable texto el tipo de error que se ha producido y el tipo de dato que es la variable resultado_tests
-      texto = `Error: resultado_tests no es un número. Tipo de dato: ${typeof resultado_tests}`;
     }
-
     memeAsync(texto_superior, texto_inferior, "Impact", 30, "")
 
       .then(json => {
-      
-       //se comprueba si la variable texto es true o false, si es true se guarda en la variable texto el valor de la variable frase_positiva, si es false se guarda en la variable texto el valor de la variable frase_negati
-        if (texto) {
-          texto = 'Los tests han pasado y lo sabes';
-        } else {
-          texto = 'Los tests han fallado y lo sabes';
-        }
-
         let readme = fs.readFileSync("README.md", "utf-8");
         readme += `<h1>${texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
         fs.writeFileSync("README.md", readme);
