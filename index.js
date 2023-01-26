9129e543f8dae5fe6c4c0150e7ad70c0bd3d385d
Jessica <jessikareix20@gmail.com>
2023-01-26 11:41:05 +0100

    workflow-action-readme

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const core = require("@actions/core");
 
 async function run() {
   try {
+//acceder desde las variables de entorno
     const frase_positiva = core.getInput("frase_positiva");
     const frase_negativa = core.getInput("frase_negativa");
-    const resultado_tests = core.getInput("resultado_tests");
+    const resultado_tests = core.getBooleanInput("resultado_tests");
+
     console.log("frase_positiva: ", frase_positiva);
     console.log("frase_negativa: ", frase_negativa);
     console.log("resultado_tests: ", resultado_tests);
@@ -20,7 +22,7 @@ function generar_meme(frase_positiva, frase_negativa, resultado_tests) {
   let texto_superior;
   let texto_inferior;
   let texto;
-  if (resultado_tests == 0) {
+  if (resultado_tests == true) {
     texto_superior = frase_positiva.split("\n")[0];
     texto=frase_positiva;
     texto_inferior = frase_positiva.split("\n")[1];
