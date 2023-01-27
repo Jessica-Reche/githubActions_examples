af1565d8c9a848a6c64c729bfdad29b92074b970
Jessica <jessikareix20@gmail.com>
2023-01-27 14:25:35 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -44595,8 +44595,6 @@ class Meme {
 
     this.texto = "";
     this.texto_inferior = "";
-    this.frase_positiva = "";
-    this.frase_positiva = core.getInput("frase_positiva");
     this.frase_negativa = core.getInput("frase_negativa");
     this.resultado_tests = parseInt(core.getInput("resultado_tests"));
     this.subreddit = "";
@@ -44608,14 +44606,12 @@ class Meme {
       if (this.frase_positiva !== 1) {
         this.subreddit = 'happy';
         this.texto = "Los tests han funcionado y lo sabes";
-        this.texto_superior = this.frase_negativa.split("\n")[0];
-        this.texto_inferior = this.frase_positiva.split("\n")[1];
+
       }
     } else {
       this.subreddit = 'sad';
-      this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto = "Los tests no han funcionado y lo sabes";
-      this.texto_inferior = this.frase_negativa.split("\n")[1];
+
     }
   }
 
@@ -44624,6 +44620,7 @@ class Meme {
     try {
       const json = await memeAsync();
       json.subreddit = this.subreddit;
+      json.title = this.subreddit;
       let readme = fs.readFileSync("README.md", "utf-8");
       readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
       fs.writeFileSync("README.md", readme);
