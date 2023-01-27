d7eb334755aa778dcbe1c81f09e2c2de8e4effa2
Jessica <jessikareix20@gmail.com>
2023-01-27 02:30:51 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -44589,40 +44589,50 @@ var __webpack_exports__ = {};
 const fs = __nccwpck_require__(7147);
 const { memeAsync } = __nccwpck_require__(3062);
 const core = __nccwpck_require__(6643);
-const readmePath = './README.md';
 
-const frase_positiva = process.env.frase_positiva || 'Los tests han funcionado y lo sabes';
-const frase_negativa = process.env.frase_negativa || 'Los tests han fallado y lo sabes';
-const resultado_tests = process.env.resultado_tests || 1;
-
-fs.readFile(readmePath, 'utf8', (err, data) => {
-  if (err) {
-    throw new Error(`Error al leer el archivo: ${err}`);
+class Meme {
+  constructor() {
+    this.texto_superior = "";
+    this.texto = "";
+    this.texto_inferior = "";
+    this.frase_positiva = core.getInput("frase_positiva");
+    this.frase_negativa = core.getInput("frase_negativa");
+    this.resultado_tests = Number(core.getInput("resultado_tests"));
   }
 
-  let newData;
-  if (resultado_tests === '0') {
-    newData = data + `\n\n${frase_positiva}`;
-  } else {
-    newData = data + `\n\n${frase_negativa}`;
-  }
-
-
-  fs.writeFile(readmePath, newData, 'utf8', (err) => {
-    if (err) {
-      throw new Error(`Error al escribir el archivo: ${err}`);
+  async testPositivo() {
+    if (this.resultado_tests === 0) {
+      this.texto_superior = this.frase_positiva.split("\n")[0];
+      this.texto = "Los tests han funcionado y lo sabes";
+      this.texto_inferior = this.frase_positiva.split("\n")[1];
     }
-    //Añadimos un meme al readme
-    memeAsync(newData,newData,"Impact",30,"" ).then(meme => {
-      fs.appendFile(readmePath, `\n\n![Meme](${meme}) <h1>${newData}</h1>`, 'utf8', (err) => {
-        if (err) {
-          throw new Error(`Error al escribir el archivo: ${err}`);
-        }
-      });
-    });
-    console.log('Meme agregado al readme con éxito');
-  });
-});
+  }
+
+  async testNegativo() {
+    if (this.resultado_tests === 1) {
+      this.texto_superior = this.frase_negativa.split("\n")[0];
+      this.texto = "Los tests no han funcionado y lo sabes";
+      this.texto_inferior = this.frase_negativa.split("\n")[1];
+    }
+  }
+
+  async run() {
+    await this.testPositivo();
+    await this.testNegativo();
+
+    await memeAsync(this.texto_superior, this.texto_inferior, "Impact", 30, "")
+      .then(json => {
+        let readme = fs.readFileSync("README.md", "utf-8");
+        readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
+        fs.writeFileSync("README.md", readme);
+        console.log("Meme añadido al readme");
+      })
+      .catch(e => console.log(e));
+  }
+}
+
+const meme = new Meme();
+meme.run();
 
 })();
 
