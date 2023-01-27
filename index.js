89a624b863a1d06cb39e8eb2ca491a7ee3f59a9a
Jessica <jessikareix20@gmail.com>
2023-01-27 02:43:16 +0100

    workflow-action-readme

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ class Meme {
     this.frase_positiva = core.getInput("frase_positiva");
     this.frase_negativa = core.getInput("frase_negativa");
     this.resultado_tests = Number(core.getInput("resultado_tests"));
-    this.subreddit = "dankmemes"; // Subreddit predeterminado
+    this.subreddit = "ProgrammerHumor"
   }
 
-  testPositivo() {
+  async testPositivo() {
     if (this.resultado_tests === 0) {
       this.texto_superior = this.frase_positiva.split("\n")[0];
       this.texto = "Los tests han funcionado y lo sabes";
@@ -21,7 +21,7 @@ class Meme {
     }
   }
 
-  testNegativo() {
+  async testNegativo() {
     if (this.resultado_tests === 1) {
       this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto = "Los tests no han funcionado y lo sabes";
@@ -30,17 +30,16 @@ class Meme {
   }
 
   async run() {
-    this.testPositivo();
-    this.testNegativo();
-
+    await this.testPositivo();
+    await this.testNegativo();
     try {
-        const json = await memeAsync(this.texto_superior, this.texto_inferior, "Impact", 30, this.subreddit);
+        const json = await memeAsync(this.texto_superior, this.texto_inferior, this.subreddit, 30, "");
         let readme = fs.readFileSync("README.md", "utf-8");
         readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
         fs.writeFileSync("README.md", readme);
         console.log("Meme añadido al readme");
     } catch (e) {
-        console.log("Error: ", e);
+        console.log(e);
     }
   }
 }
