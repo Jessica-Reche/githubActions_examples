bb97a54a3239053c5c4147987d050b44bcacdca0
Jessica <jessikareix20@gmail.com>
2023-01-28 00:01:37 +0100

    workflow-action-readme

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Meme {
      
       json.title = this.subreddit;
       let readme = fs.readFileSync("README.md", "utf-8");
-      readme = `<h1>${ this.texto = this.resultado_tests === 1 ? this.texto = "Los tests han funcionado y lo sabes" : this.texto = "Los tests no han funcionado y lo sabes"}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
+      readme = `<h1>${ this.texto = this.resultado_tests ? this.texto = "Los tests no han funcionado y lo sabes" : this.texto = "Los tests han fallado funcionado y lo sabes"}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
       fs.writeFileSync("README.md", readme);
       console.log("Meme añadido al readme");
     } catch (e) {
