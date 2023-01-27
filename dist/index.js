20ccea226354bb51ce9eb41ee4f2640c223e914a
Jessica <jessikareix20@gmail.com>
2023-01-27 03:31:11 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -44592,9 +44592,10 @@ const core = __nccwpck_require__(6643);
 
 class Meme {
   constructor() {
-   
+
     this.texto = "";
     this.texto_inferior = "";
+    this.frase_positiva = "";
     this.frase_positiva = core.getInput("frase_positiva");
     this.frase_negativa = core.getInput("frase_negativa");
     this.resultado_tests = Number(core.getInput("resultado_tests"));
@@ -44603,15 +44604,18 @@ class Meme {
 
   async testPositivo() {
     if (this.resultado_tests === 0) {
-      this.subreddit ='happy';
+      this.subreddit = 'happy';
       this.texto = "Los tests han funcionado y lo sabes";
+      this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto_inferior = this.frase_positiva.split("\n")[1];
+
     }
   }
 
   async testNegativo() {
     if (this.resultado_tests === 1) {
-      this.subreddit ='sad';
+      this.subreddit = 'sad';
+      this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto = "Los tests no han funcionado y lo sabes";
       this.texto_inferior = this.frase_negativa.split("\n")[1];
     }
@@ -44621,20 +44625,24 @@ class Meme {
     await this.testPositivo();
     await this.testNegativo();
     try {
-        const json = await memeAsync( this.subreddit);
-        let readme = fs.readFileSync("README.md", "utf-8");
-        readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
-        fs.writeFileSync("README.md", readme);
-        console.log("Meme añadido al readme");
+      const json = await memeAsync();
+      json.catch((error) => {
+        console.log(error);
+      })
+      json.subreddit = this.subreddit;
+      json.title = this.subreddit;
+      let readme = fs.readFileSync("README.md", "utf-8");
+      readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
+      fs.writeFileSync("README.md", readme);
+      console.log("Meme añadido al readme");
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
   }
 }
 
 const meme = new Meme();
 meme.run();
-
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const core = require("@actions/core");
 
 class Meme {
   constructor() {
-   
+
     this.texto = "";
     this.texto_inferior = "";
+    this.frase_positiva = "";
     this.frase_positiva = core.getInput("frase_positiva");
     this.frase_negativa = core.getInput("frase_negativa");
     this.resultado_tests = Number(core.getInput("resultado_tests"));
@@ -15,15 +16,18 @@ class Meme {
 
   async testPositivo() {
     if (this.resultado_tests === 0) {
-      this.subreddit ='happy';
+      this.subreddit = 'happy';
       this.texto = "Los tests han funcionado y lo sabes";
+      this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto_inferior = this.frase_positiva.split("\n")[1];
+
     }
   }
 
   async testNegativo() {
     if (this.resultado_tests === 1) {
-      this.subreddit ='sad';
+      this.subreddit = 'sad';
+      this.texto_superior = this.frase_negativa.split("\n")[0];
       this.texto = "Los tests no han funcionado y lo sabes";
       this.texto_inferior = this.frase_negativa.split("\n")[1];
     }
@@ -33,13 +37,18 @@ class Meme {
     await this.testPositivo();
     await this.testNegativo();
     try {
-        const json = await memeAsync( this.subreddit);
-        let readme = fs.readFileSync("README.md", "utf-8");
-        readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
-        fs.writeFileSync("README.md", readme);
-        console.log("Meme añadido al readme");
+      const json = await memeAsync();
+      json.catch((error) => {
+        console.log(error);
+      })
+      json.subreddit = this.subreddit;
+      json.title = this.subreddit;
+      let readme = fs.readFileSync("README.md", "utf-8");
+      readme += `<h1>${this.texto}</h1> <img src="${json.url}" alt="meme" width="500" height="500"></img>`;
+      fs.writeFileSync("README.md", readme);
+      console.log("Meme añadido al readme");
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
   }
 }
