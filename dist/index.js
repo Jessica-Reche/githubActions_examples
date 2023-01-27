34a866fcf88a4cb25098ef4292ba94ea60bfddd3
Jessica <jessikareix20@gmail.com>
2023-01-27 12:53:25 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -5720,9 +5720,10 @@ module.exports.implForWrapper = function (wrapper) {
 /***/ 6365:
 /***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
+var __webpack_unused_export__;
 const fetch = __nccwpck_require__(8574);
 
-exports.requestmeme = function(what) {
+__webpack_unused_export__ = function(what) {
   return new Promise((resolve, reject) => {
     fetch("https://meme-api.herokuapp.com/gimme")
       .then(res => res.json())
@@ -6847,7 +6848,7 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const fs = __nccwpck_require__(7147);
-const nodeMeme = __nccwpck_require__(6365);
+const nodeMeme = (__nccwpck_require__(6365)["default"]);
 const core = __nccwpck_require__(6643);
 
 class Meme {
