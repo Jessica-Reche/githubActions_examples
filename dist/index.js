f91cd95c4defa80a0beab9a3911327975c084809
Jessica <jessikareix20@gmail.com>
2023-01-27 13:02:43 +0100

    workflow-action-readme

--- dist/index.js
+++ dist/index.js
@@ -6863,35 +6863,35 @@ class Meme {
   }
   async test() {
     if (this.resultado_tests === 0) {
-        this.meme_name = 'happy';
-        this.texto = "Los tests han funcionado y lo sabes";
-        this.texto_superior = this.frase_negativa.split("\n")[0];
-        this.texto_inferior = this.frase_positiva.split("\n")[1];
+      this.meme_name = 'happy';
+      this.texto = "Los tests han funcionado y lo sabes";
+      this.texto_superior = this.frase_negativa.split("\n")[0];
+      this.texto_inferior = this.frase_positiva.split("\n")[1];
     } else {
-        this.meme_name = 'sad';
-        this.texto_superior = this.frase_negativa.split("\n")[0];
-        this.texto = "Los tests no han funcionado y lo sabes";
-        this.texto_inferior = this.frase_negativa.split("\n")[1];
+      this.meme_name = 'sad';
+      this.texto_superior = this.frase_negativa.split("\n")[0];
+      this.texto = "Los tests no han funcionado y lo sabes";
+      this.texto_inferior = this.frase_negativa.split("\n")[1];
     }
-}
+  }
 
   async run() {
-   await this.test();
+    await this.test();
     try {
-     
+
       nodeMeme.requestmeme(this.meme_name).then(img => {
         console.log(img);
         readme += `<h1>${this.texto}</h1> <img src="${img}" alt="meme" width="500" height="500"></img>`;
         let readme = fs.readFileSync("README.md", "utf-8");
         fs.writeFileSync("README.md", readme);
-      console.log("Meme añadido al readme");
-    }).catch(err => {
-      console.log(err);
-    });
-      
-      
+        console.log("Meme añadido al readme");
+      }).catch(err => {
+        throw new Error(err);
+      });
+
+
     } catch (e) {
-      console.log(e); 
+      throw new Error(e);
     }
   }
 }
